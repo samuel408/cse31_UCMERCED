f62d9b2aa9baa3e7331dfa4b42396c5dc19ea819
Samuel <samuel@Samuels-MacBook-Pro.local>
2022-02-10 22:28:48 -0800

    finished average.c

--- Lab_02/average.c
+++ Lab_02/average.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /*
     Read a set of values from the user.
     Store the sum in the sum variable and return the number of values read.
@@ -7,14 +6,14 @@
 
 // You CANNOT declare any global  variables
 // You CANNOT change the definition of the read_values function (e.g., add/remove function parameters, or change the return type)
-int read_values(double sum) {
+int read_values(double *sum) {// make sum a pointer parameter
   int values = 0, input = 0;
-  sum = 0;
+  (*sum) = 0;
   printf("Enter input values (enter 0 to finish):\n");
   scanf("%d", &input);
   while(input != 0) {
     values++;
-    sum += input;
+    (*sum) += input;//make sure to change sum into a pointer to the match the parameter
     scanf("%d", &input);
   }
   return values;
@@ -23,10 +22,9 @@ int read_values(double sum) {
 int main() {
   double sum = 0;
   int values;
-  values = read_values(sum);
-  double average = sum/values;
-  printf("\nAverage: %1f\n", &average); // Hint: How do we ensure that sum is updated here AFTER read_value manipulates it?
+  double *sumNew = &sum;// initialize a pointer and make it equal what is stored in the memory of sum.
+  values = read_values(sumNew);
+  printf("\nAverage: %g\n", sum/values); // Hint: How do we ensure that sum is updated here AFTER read_value manipulates it?
   return 0;
 }
 
- 
