a84f8c87a7296f07c317af034808a6212d0c8185
Samuel <samuel@Samuels-MacBook-Pro.local>
2022-03-03 21:35:59 -0800

    finished memcast.c

--- Lab_04/MemCast.c
+++ Lab_04/MemCast.c
@@ -16,6 +16,21 @@ int main() {
         four_c[i] = 'A' + i; // ASCII value of 'A' is 65 or 0x41 in Hex.
     
     // Add your code for the exercises here:
+
+	printf("%x\n",four_ints[0]);
+	printf("four_ints: %p four_c:",four_ints, four_c);
+	printf("\n");
+	for(int i = 0; i < 4; i++){
+		printf("element no. %d address: %p value: ", i, four_ints+i, *(four_ints + i));
+		printf("\n");
+	}
+	for(int i = 0; i < 4; i++){
+		printf("element no. %d address: %p value: ", i, four_c+i, *(four_c + i));
+		printf("\n");
+	}
+
+
+	
 	
 	return 0;
 }
